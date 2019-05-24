43d77341e62ef1b324c9a876ef23bdbe6c243dc9
W_LIURUNKAI <aN7cU5nU>
2019-05-24 11:27:12 +0800

    修改flyway初始化user表数据

---
+++ flyway/src/main/resources/db/migration/V1.1__init_data.sql
@@ -0,0 +1,3 @@
+insert into  user(id,user_name,name,age,balance) values (1,"accout1","张三",28,120);
+insert into  user(id,user_name,name,age,balance) values (2,"accout2","李四",30,150);
+insert into  user(id,user_name,name,age,balance) values (3,"accout3","王五",22,80);
