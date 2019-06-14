bf3ef592891260a0677d11039b2952d5e4a001ad
W_LIURUNKAI <aN7cU5nU>
2019-06-14 19:17:15 +0800

    动态添加和取消定时任务

--- dynamic-more-schedule/src/main/java/com/liu/sc/controller/UserController.java
+++ dynamic-more-schedule/src/main/java/com/liu/sc/controller/UserController.java
@@ -6,13 +6,18 @@ import com.liu.sc.service.CronService;
 import com.liu.sc.service.UserService;
 import com.liu.sc.utils.DefaultSchedulingConfigurer;
 import lombok.extern.slf4j.Slf4j;
+import org.quartz.CronExpression;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.SchedulingException;
+import org.springframework.scheduling.Trigger;
+import org.springframework.scheduling.TriggerContext;
 import org.springframework.scheduling.config.TriggerTask;
 import org.springframework.scheduling.support.CronTrigger;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
 import java.util.Set;
 
@@ -44,7 +49,6 @@ public class UserController {
 
     @GetMapping("/testDynamicSchedule")
     public void testDynamicSchedule(){
-        Cron cron1 = cronService.getCronByType(1);
         User user = new User();
         Long balance = 1L;
         user.setBalance(BigDecimal.valueOf(balance));
@@ -56,9 +60,23 @@ public class UserController {
                     @Override
                     public void run() {
                         User user1 = userService.addUser(user);
-                        log.info("userId is {}",user1.getId());
+                        log.info("userId is {}", user1.getId());
                     }
-                }, new CronTrigger(cron1.getCron())));
+                }, new Trigger() {
+                    @Override
+                    public Date nextExecutionTime(TriggerContext triggerContext) {
+                        Cron cron1 = cronService.getCronByType(1);
+                        // 验证cron表达式
+                        if("".equals(cron1.getCron()) || cron1.getCron() == null){
+                            throw new SchedulingException("cron is null");
+                        }
+                        CronExpression.isValidExpression(cron1.getCron());
+                        //定时任务触发，可修改定时任务的执行周期
+                        CronTrigger cronTrigger = new CronTrigger(cron1.getCron());
+                        Date nextExecutionTime = cronTrigger.nextExecutionTime(triggerContext);
+                        return nextExecutionTime;
+                    }
+                }));
     }
 
     @GetMapping("/testDynamicSchedule2")
@@ -84,7 +102,11 @@ public class UserController {
     public void testCancelSchedule(){
         Set<String> ids = defaultSchedulingConfigurer.taskIds();
         log.info("ids is {}",ids);
-        defaultSchedulingConfigurer.cancelTriggerTask(String.valueOf(2));
+        defaultSchedulingConfigurer.cancelTriggerTask(String.valueOf(1));
     }
 
+    public static void main(String[] args) {
+        boolean b = CronExpression.isValidExpression("0/5 * * * * ?");
+        System.out.println(b);
+    }
 }
