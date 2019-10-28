fd6e025a965d598e11c67a98de01d390b012667d
w_liurunkai <Lrk291988731>
2019-10-28 21:25:49 +0800

    dubbo学习

--- dubbo-order-service-comsumer/src/main/java/com/liu/dubbo/DubboOrderServiceComsumerApplication.java
+++ dubbo-order-service-comsumer/src/main/java/com/liu/dubbo/DubboOrderServiceComsumerApplication.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.ImportResource;
  * dubbo整合springBoot的方式
  * （1）使用@service注解暴露服务和@Reference注解调用服务，启动类里面需要添加@enableDubbo注解
  * （2）使用xml的配置文件，需要在启动类里面添加@ImportResource注解
- * （3）使用api的方式
+ * （3）使用api的方式，见dubboConfig，需要使用@EnableDubbo注解(scanBasePackages="com.liu.dubbo")
  */
 @EnableDubbo
 //@ImportResource(locations = "classpath:consumer.xml")
