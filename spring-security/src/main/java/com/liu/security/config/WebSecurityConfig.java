69b54631a77e0234f8d5fbf970e63d0df7b7dcb1
liurunkai <liurunkai@chinadatagroup.com>
2020-01-16 17:35:10 +0800

    update

--- spring-security/src/main/java/com/liu/security/config/WebSecurityConfig.java
+++ spring-security/src/main/java/com/liu/security/config/WebSecurityConfig.java
@@ -1,10 +1,14 @@
 package com.liu.security.config;
 
+import com.liu.security.service.impl.SysUserServiceImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 /**
  * @Description:
@@ -16,11 +20,22 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableGlobalMethodSecurity(prePostEnabled = true) //开启方法权限控制
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    UserDetailsService getUserService() {
+        return new SysUserServiceImpl();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(getUserService());
+    }
+
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeRequests()
                 .antMatchers("/css/**","/js/**").permitAll()
-                .antMatchers("/login/**").permitAll();
+                .anyRequest().authenticated()
+                .and().formLogin().loginPage("/login").failureForwardUrl("");
     }
 
 }
